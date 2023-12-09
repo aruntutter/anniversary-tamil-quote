8e605ac1137c67057524187c3f704ddbf6cee832
Arun Kumar R <aruntutter19@gmail.com>
2023-12-09 23:00:05 +0530

    audio file and UI updated

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const quotes = document.querySelectorAll(".quote");
-  const totalQuotes = quotes.length;
+  const audio = document.getElementById("backgroundMusic");
+  const playButton = document.getElementById("playButton");
+  const volumeControl = document.getElementById("volumeControl");
 
-  function animateQuote(index, direction) {
-    const quote = quotes[index];
-    const startPosition = direction === "right" ? "100%" : "-100%";
-    const endPosition = "0";
-
-    quote.style.opacity = "0";
-    quote.style.transform = `translateX(${startPosition})`;
-    quote.style.transition = "opacity 1s, transform 1s";
-
-    // Triggering a reflow to restart the transition
-    void quote.offsetWidth;
-
-    setTimeout(() => {
-      quote.style.opacity = "1";
-      quote.style.transform = `translateX(${endPosition})`;
-    }, index * 700); // Delay each quote by 2 seconds
-  }
-
-  // Initially hide all quotes
-  quotes.forEach((quote) => {
-    quote.style.opacity = "0";
+  // Play audio on play button click
+  playButton.addEventListener("click", function () {
+    if (audio.paused) {
+      audio.play();
+      playButton.textContent = "Pause";
+    } else {
+      audio.pause();
+      playButton.textContent = "Play";
+    }
   });
 
-  // Animation sequence
-  animateQuote(0, "right"); // quote1 from the right
-  animateQuote(1, "left"); // quote2 from the left
-  animateQuote(2, "right"); // quote3 from the right
-  animateQuote(3, "left"); // quote4 from the left
-  animateQuote(4, "right"); // quote5 from the right
+  // Adjust volume on volume control change
+  volumeControl.addEventListener("input", function () {
+    audio.volume = volumeControl.value;
+  });
 });
