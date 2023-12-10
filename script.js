b8ca16b2888ccf5333ef0e97ece4eb71b374457e
Arun Kumar R <aruntutter19@gmail.com>
2023-12-10 15:51:07 +0530

    ui and animation updated

--- script.js
+++ script.js
@@ -1,13 +1,153 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const audio = document.getElementById("backgroundMusic");
+//   const playButton = document.getElementById("playButton");
+//   const volumeControl = document.getElementById("volumeControl");
+
+//   // Play audio on play button click
+//   playButton.addEventListener("click", function () {
+//     if (audio.paused) {
+//       audio.play();
+//       playButton.textContent = "Pause";
+//     } else {
+//       audio.pause();
+//       playButton.textContent = "Play";
+//     }
+//   });
+
+//   // Adjust volume on volume control change
+//   volumeControl.addEventListener("input", function () {
+//     audio.volume = volumeControl.value;
+//   });
+// });
+
+// // Animation
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const audio = document.getElementById("backgroundMusic");
+//   const playButton = document.getElementById("playButton");
+//   const volumeControl = document.getElementById("volumeControl");
+//   const quoteContainer = document.getElementById("quote2");
+
+//   const words = [
+//     "😤😤😤😤",
+//     "Dai",
+//     "😂😂",
+//     "Arun Sunn",
+//     "🥳",
+//     "Love you 😘",
+//     "😍😚😚",
+//     "bs podum",
+//     "bhgg😤😤",
+//     "kutte",
+//     "🍑",
+//     "Arunnnn",
+//     "🥹🥹🥹",
+//     "babe",
+//     "🙄🙄",
+//     "motti",
+//     "😁",
+//     "babes",
+//     "😤😤😤",
+//     "thango",
+//     "mooditu velai paaru",
+//     "😤",
+//     "devatha",
+//     "chubby cheeks",
+//     "😍",
+//     "poda dai",
+//     "kiss kuda pannule arun nee",
+//     "😤😤😤",
+//     "maiyuru",
+//     "😘",
+//     "moodu",
+//     "serupu",
+//     "🤤",
+//     "👉🍑👈",
+//     "Arun no 😤",
+//     "🥹",
+//     "what babes",
+//     "madhu👉👈",
+//     "wo🥹",
+//     "arun chup",
+//     "🥺☹️",
+//     "think h na",
+//     "chi poo",
+//   ];
+
+//   let currentIndex = 0;
+
+//   function displayNextWord() {
+//     quoteContainer.innerHTML = `"${words[currentIndex]}"`;
+//     currentIndex = (currentIndex + 1) % words.length;
+//   }
+
+//   setInterval(displayNextWord, 2000); // Change the interval as needed (in milliseconds)
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const audio = document.getElementById("backgroundMusic");
   const playButton = document.getElementById("playButton");
   const volumeControl = document.getElementById("volumeControl");
+  const quoteContainer = document.getElementById("quote2");
+
+  const words = [
+    "😤😤😤😤",
+    "Dai",
+    "😂😂",
+    "Arun Sunn",
+    "🥳",
+    "Love you 😘",
+    "😍😚😚",
+    "bs podum",
+    "bhgg😤😤",
+    "kutte",
+    "🍑",
+    "Arunnnn",
+    "🥹🥹🥹",
+    "babe",
+    "🙄🙄",
+    "motti",
+    "😁",
+    "babes",
+    "😤😤😤",
+    "thango",
+    "mooditu velai paaru",
+    "😤",
+    "devatha",
+    "chubby cheeks",
+    "😍",
+    "poda dai",
+    "kiss kuda pannule arun nee",
+    "😤😤😤",
+    "maiyuru",
+    "😘",
+    "moodu",
+    "serupu",
+    "🤤",
+    "🍑",
+    "Arun no 😤",
+    "🥹",
+    "what babes",
+    "madhu👉",
+    "wo🥹",
+    "arun chup",
+    "🥺☹️",
+    "think h na",
+    "chi poo",
+  ];
+
+  let currentIndex = 0;
+  let animationStarted = false;
 
   // Play audio on play button click
   playButton.addEventListener("click", function () {
     if (audio.paused) {
       audio.play();
       playButton.textContent = "Pause";
+      if (!animationStarted) {
+        setInterval(displayNextWord, 2000); // Change the interval as needed (in milliseconds)
+        animationStarted = true;
+      }
     } else {
       audio.pause();
       playButton.textContent = "Play";
@@ -18,4 +158,9 @@ document.addEventListener("DOMContentLoaded", function () {
   volumeControl.addEventListener("input", function () {
     audio.volume = volumeControl.value;
   });
+
+  function displayNextWord() {
+    quoteContainer.innerHTML = `👉${words[currentIndex]}👈`;
+    currentIndex = (currentIndex + 1) % words.length;
+  }
 });
